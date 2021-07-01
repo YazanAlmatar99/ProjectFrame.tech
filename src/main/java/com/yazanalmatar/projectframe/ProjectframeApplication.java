04699629ca5f76091840bf603cb1d48b365cd17e
Yazan <yazana@ilsny.com>
2021-07-01 00:54:18 -0400

    initial commit

---
+++ src/main/java/com/yazanalmatar/projectframe/ProjectframeApplication.java
@@ -0,0 +1,13 @@
+package com.yazanalmatar.projectframe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectframeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectframeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yazanalmatar/projectframe/ProjectframeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yazanalmatar.projectframe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectframeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
