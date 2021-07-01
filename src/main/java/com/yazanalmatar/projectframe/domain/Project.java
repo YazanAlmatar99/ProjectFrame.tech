bfe0add6b922ee71af9a6563c815a2010ba28f10
Yazan <yazana@ilsny.com>
2021-07-01 02:27:17 -0400

    Added initial validation for Project attributes. Closes #3.

--- src/main/java/com/yazanalmatar/projectframe/domain/Project.java
+++ src/main/java/com/yazanalmatar/projectframe/domain/Project.java
@@ -1,6 +1,10 @@
 package com.yazanalmatar.projectframe.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.Date;
 
 @Entity
@@ -9,14 +13,25 @@ public class Project {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank(message = "Project name is required")
     private String projectName;
+
+    @NotBlank(message = "Project Identifier is required")
+    @Size(min = 4, max = 6, message = "Please use 4 to 6 characters")
+    @Column(updatable = false, unique = true)
     private String projectIdentifier;
+
+    @NotBlank(message = "Project description is required")
     private String description;
+
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date start_date;
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date end_date;
 
-
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date created_At;
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date updated_At;
 
 
