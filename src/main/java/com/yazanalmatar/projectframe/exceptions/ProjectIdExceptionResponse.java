6171b2ae746fe1585e36ef464de490f89b4c18b7
Yazan <yazana@ilsny.com>
2021-07-02 13:15:01 -0400

    Created custom exception handler and response. Closes #5.

--- src/main/java/com/yazanalmatar/projectframe/exceptions/ProjectIdExceptionResponse.java
+++ src/main/java/com/yazanalmatar/projectframe/exceptions/ProjectIdExceptionResponse.java
@@ -1,2 +1,18 @@
-package com.yazanalmatar.projectframe.exceptions;public class ProjectIdExceptionResponse {
+package com.yazanalmatar.projectframe.exceptions;
+
+public class ProjectIdExceptionResponse {
+
+    private String projectIdentifier;
+
+    public ProjectIdExceptionResponse(String projectIdentifier) {
+        this.projectIdentifier = projectIdentifier;
+    }
+
+    public String getProjectIdentifier() {
+        return projectIdentifier;
+    }
+
+    public void setProjectIdentifier(String projectIdentifier) {
+        this.projectIdentifier = projectIdentifier;
+    }
 }
--- src/main/java/com/yazanalmatar/projectframe/services/ProjectService.java
+++ src/main/java/com/yazanalmatar/projectframe/services/ProjectService.java
@@ -1,6 +1,7 @@
 package com.yazanalmatar.projectframe.services;
 
 import com.yazanalmatar.projectframe.domain.Project;
+import com.yazanalmatar.projectframe.exceptions.ProjectIdException;
 import com.yazanalmatar.projectframe.repositories.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,10 +12,15 @@ public class ProjectService {
     private ProjectRepository projectRepository;
 
     public Project saveOrUpdateProject(Project project) {
-
+        try {
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            return projectRepository.save(project);
+        } catch (Exception e) {
+            throw new ProjectIdException("Project ID " + project.getProjectIdentifier().toLowerCase() + " already exists");
+        }
         //TODO determine whether the user is the owner if the project or no
 
-        return projectRepository.save(project);
+
     }
 
 }
