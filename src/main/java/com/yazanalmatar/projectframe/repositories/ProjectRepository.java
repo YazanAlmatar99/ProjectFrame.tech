c9c047db1627cfa6c5d92dc1aa0db4d67aabc6e0
Yazan <yazana@ilsny.com>
2021-07-02 14:02:34 -0400

    Created getAllProjects route. Closes #7.

--- src/main/java/com/yazanalmatar/projectframe/repositories/ProjectRepository.java
+++ src/main/java/com/yazanalmatar/projectframe/repositories/ProjectRepository.java
@@ -10,4 +10,7 @@ public interface ProjectRepository extends CrudRepository<Project, Long> {
     Iterable<Project> findAllById(Iterable<Long> iterable);
 
     Project findByProjectIdentifier(String projectId);
+
+    @Override
+    Iterable<Project> findAll();
 }
