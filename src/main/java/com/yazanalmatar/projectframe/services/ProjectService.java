189306a6d2f749f0f20163d86a6215501d7e7b15
Yazan <yazana@ilsny.com>
2021-07-02 14:11:03 -0400

    Created deleteProjectByIdentifier route. Closes #8.

--- src/main/java/com/yazanalmatar/projectframe/services/ProjectService.java
+++ src/main/java/com/yazanalmatar/projectframe/services/ProjectService.java
@@ -35,4 +35,13 @@ public class ProjectService {
         return projectRepository.findAll();
     }
 
+    public void deleteProjectByIdentifier(String projectId) {
+        Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+
+        if (project == null) {
+            throw new ProjectIdException("Cannot Delete Project with ID " + projectId + ". This project does not exist");
+        }
+        projectRepository.delete(project);
+    }
+
 }
--- src/main/java/com/yazanalmatar/projectframe/web/ProjectController.java
+++ src/main/java/com/yazanalmatar/projectframe/web/ProjectController.java
@@ -44,4 +44,11 @@ public class ProjectController {
     public Iterable<Project> getAllProjects() {
         return projectService.findAllProjects();
     }
+
+    @DeleteMapping("/{projectId}")
+    public ResponseEntity<?> deleteProjectByIdentifier(@PathVariable String projectId) {
+        projectService.deleteProjectByIdentifier(projectId);
+
+        return new ResponseEntity<String>("Project with ID: " + projectId + " was deleted", HttpStatus.OK);
+    }
 }
