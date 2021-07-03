f23040ee15d51eba81ce2969e3e15fa97edbd43d
Yazan <yazana@ilsny.com>
2021-07-03 19:32:51 -0400

    Configured web securiy. Closes #23.

---
+++ src/main/java/com/yazanalmatar/projectframe/repositories/UserRepository.java
@@ -0,0 +1,8 @@
+package com.yazanalmatar.projectframe.repositories;
+
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UserRepository {
+}
---
+++ src/main/java/com/yazanalmatar/projectframe/web/UserController.java
@@ -0,0 +1,9 @@
+package com.yazanalmatar.projectframe.web;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+public class UserController {
+}
