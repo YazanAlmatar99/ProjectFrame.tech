28587acfd471277502ff2c78179ec8c599a39046
Yazan <yazana@ilsny.com>
2021-07-05 14:29:33 -0400

    Configure Login Manager

--- src/main/java/com/yazanalmatar/projectframe/ProjectframeApplication.java
+++ src/main/java/com/yazanalmatar/projectframe/ProjectframeApplication.java
@@ -2,12 +2,18 @@ package com.yazanalmatar.projectframe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class ProjectframeApplication {
+    @Bean
+    BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProjectframeApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectframeApplication.class, args);
+    }
 
 }
--- src/main/java/com/yazanalmatar/projectframe/security/WebSecurityConfig.java
+++ src/main/java/com/yazanalmatar/projectframe/security/WebSecurityConfig.java
@@ -1,13 +1,19 @@
 package com.yazanalmatar.projectframe.security;
 
 
+import com.yazanalmatar.projectframe.services.CustomUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.BeanIds;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -20,7 +26,21 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private JwtAuthenticationEntryPoint unauthorizedHandler;
+    @Autowired
+    private CustomUserDetailsService customUserDetailsService;
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
 
+    @Override
+    protected void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+        authenticationManagerBuilder.userDetailsService(customUserDetailsService).passwordEncoder(passwordEncoder);
+    }
+
+    @Override
+    @Bean(BeanIds.AUTHENTICATION_MANAGER)
+    protected AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -45,6 +65,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                         "/**/*.css",
                         "/**/*.js"
                 ).permitAll()
+                .antMatchers("/api/users/**").permitAll()
+                .antMatchers("/h2-console/**").permitAll()
                 .anyRequest().authenticated(); //Any other request will have to be authenticated
     }
 }
