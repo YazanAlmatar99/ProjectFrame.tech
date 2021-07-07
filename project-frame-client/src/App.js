5e7cc84bbce0ad299dfcca38cb3786d154adcd40
Yazan <yazana@ilsny.com>
2021-07-07 15:50:08 -0400

    clean up

--- project-frame-client/src/App.js
+++ project-frame-client/src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Dashboard from "./components/Dashboard";
+import Header from "./components/Layout/Header";
+import AddProject from "./components/Project/AddProject";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Route path="/dashboard" component={Dashboard} exact />
+        <Route path="/addProject" component={AddProject} exact />
+      </div>
+    </Router>
   );
 }
 
