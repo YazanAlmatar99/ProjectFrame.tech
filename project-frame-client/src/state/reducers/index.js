0b209b94b8438d3bd17d099e39ae0738d64ea1be
Yazan <yazana@ilsny.com>
2021-07-10 18:17:07 -0400

    enabled cors on projectController

--- project-frame-client/src/state/reducers/index.js
+++ project-frame-client/src/state/reducers/index.js
@@ -1,5 +1,7 @@
 import { combineReducers } from "redux";
 import { projectReducer } from "./projectReducer";
+import { errorReducer } from "./errorReducer";
 export default combineReducers({
   projectState: projectReducer,
+  errorState: errorReducer,
 });
