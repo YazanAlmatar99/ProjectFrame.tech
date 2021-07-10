bbce070871f5bf9e2299cf6c8f2d5e44e5737829
Yazan <yazana@ilsny.com>
2021-07-10 18:15:45 -0400

    configured react-toastify

--- project-frame-client/src/App.js
+++ project-frame-client/src/App.js
@@ -3,11 +3,13 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Dashboard from "./components/Dashboard";
 import Header from "./components/Layout/Header";
 import AddProject from "./components/Project/AddProject";
+import { ToastContainer, toast } from "react-toastify";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 function App() {
   return (
     <Router>
       <div className="App">
+        <ToastContainer autoClose={2500} draggable pauseOnHover />
         <Header />
         <Route path="/dashboard" component={Dashboard} exact />
         <Route path="/addProject" component={AddProject} exact />
