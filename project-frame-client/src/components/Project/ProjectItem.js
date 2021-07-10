e6c066fceda22e81cf4883240ca63883d352aba1
Yazan <yazana@ilsny.com>
2021-07-10 18:17:57 -0400

    created ProjectItem Component. Closes #44

--- project-frame-client/src/components/Project/ProjectItem.js
+++ project-frame-client/src/components/Project/ProjectItem.js
@@ -1,34 +1,44 @@
 import React, { useState } from "react";
+import CreateProjectButton from "./CreateProjectButton";
+import { useActions } from "../../hooks/use-actions";
+import "react-toastify/dist/ReactToastify.css";
+import { Link } from "react-router-dom";
+const ProjectItem = ({ project }) => {
+  const { deleteProject } = useActions();
 
-const ProjectItem = () => {
+  const onDeleteClick = (projectIdentifier) => {
+    deleteProject(projectIdentifier);
+  };
   return (
     <div className="container">
       <div className="card card-body bg-light mb-3">
         <div className="row">
           <div className="col-2">
-            <span className="mx-auto">REACT</span>
+            <span className="mx-auto">{project.projectIdentifier}</span>
           </div>
           <div className="col-lg-6 col-md-4 col-8">
-            <h3>Spring / React Project</h3>
-            <p>Project to create a Kanban Board with Spring Boot and React</p>
+            <h3>{project.projectName}</h3>
+            <p>{project.description}</p>
           </div>
           <div className="col-md-4 d-none d-lg-block">
             <ul className="list-group">
-              <a href="#">
+              <Link>
                 <li className="list-group-item board">
                   <i className="fa fa-flag-checkered pr-1"> Project Board </i>
                 </li>
-              </a>
-              <a href="#">
+              </Link>
+              <Link to={`/updateProject/${project.projectIdentifier}`}>
                 <li className="list-group-item update">
                   <i className="fa fa-edit pr-1"> Update Project Info</i>
                 </li>
-              </a>
-              <a href="">
-                <li className="list-group-item delete">
-                  <i className="fa fa-minus-circle pr-1"> Delete Project</i>
-                </li>
-              </a>
+              </Link>
+
+              <li
+                className="list-group-item delete"
+                onClick={() => onDeleteClick(project.projectIdentifier)}
+              >
+                <i className="fa fa-minus-circle pr-1"> Delete Project</i>
+              </li>
             </ul>
           </div>
         </div>
