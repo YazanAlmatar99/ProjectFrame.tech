e0ad4fda4d1037c1a80e9c858376bc32efadca4c
Yazan <yazana@ilsny.com>
2021-07-12 23:55:22 -0400

    added error handling to project actions

--- project-frame-client/src/state/actions/projectActions.js
+++ project-frame-client/src/state/actions/projectActions.js
@@ -7,45 +7,40 @@ import {
   GET_ERRORS,
 } from "../action-types/index";
 const token =
-  "Bearer eyJhbGciOiJIUzUxMiJ9.eyJmdWxsTmFtZSI6IllhemFuIEFsbWF0YXIiLCJpZCI6IjEiLCJleHAiOjE2MjU5NzYxOTcsImlhdCI6MTYyNTk0NjE5NywidXNlcm5hbWUiOiJtZUB5YXphbmFsbWF0YXIuY29tIn0.iZhsBijirdNUNUPQtvfuKgco375FKOPVJl7CqXENEvKUyaG8xpwGdgTjONOR5ypYN1oeNrKz16hMfpVBFhWojQ";
+  "Bearer eyJhbGciOiJIUzUxMiJ9.eyJmdWxsTmFtZSI6IllhemFuIEFsbWF0YXIiLCJpZCI6IjEiLCJleHAiOjE2MjYxNDY1MTYsImlhdCI6MTYyNjExNjUxNiwidXNlcm5hbWUiOiJtZUB5YXphbmFsbWF0YXIuY29tIn0.9eYb0oUPCqeMu9LHVClqG0kXyrMC1BSHI2DFMnVUmH05g8_JDXtsYMXh4nfUUXMVpshIRfJVwOfKd-z3V55VQg";
 export const createProject = (project, history) => async (dispatch) => {
   console.log(project, "THS IS THE UPDATE");
   try {
-    const response = await axios.post(
-      "/api/project",
-      project,
-
-      {
-        headers: {
-          Authorization: token,
-        },
-      }
-    );
+    const response = await axios.post("/api/project", project);
     dispatch({ type: GET_ERRORS, payload: {} });
-    toast.success("Project Was Successfully Created!");
+    if (!project.id) {
+      toast.success("Project Was Successfully Created!");
+    } else {
+      toast.success("Project Was Successfully Updated!");
+    }
+
     history.push("/dashboard");
   } catch (error) {
     dispatch({ type: GET_ERRORS, payload: error.response.data });
+    toast.error("Failed Creating a Project!");
   }
 };
 
 export const getProjects = () => async (dispatch) => {
-  const response = await axios.get("/api/project/all", {
-    headers: {
-      Authorization: token,
-    },
-  });
-  dispatch({ type: GET_PROJECTS, payload: response.data });
+  try {
+    const response = await axios.get("/api/project/all");
+    dispatch({ type: GET_PROJECTS, payload: response.data });
+  } catch (error) {
+    toast.error(
+      "Failed loading Projects! Status code: " + error.response.status
+    );
+  }
 };
 
 export const getProjectByIdentifier =
   (projectIdentifier, history) => async (dispatch) => {
     try {
-      const response = await axios.get(`/api/project/${projectIdentifier}`, {
-        headers: {
-          Authorization: token,
-        },
-      });
+      const response = await axios.get(`/api/project/${projectIdentifier}`);
 
       dispatch({ type: GET_PROJECT, payload: response.data });
     } catch (error) {
@@ -62,11 +57,7 @@ export const deleteProject = (projectIdentifier) => async (dispatch) => {
     )
   ) {
     try {
-      const response = await axios.delete(`/api/project/${projectIdentifier}`, {
-        headers: {
-          Authorization: token,
-        },
-      });
+      const response = await axios.delete(`/api/project/${projectIdentifier}`);
       dispatch({ type: DELETE_PROJECT, payload: projectIdentifier });
       toast.success("Project Was Successfully Deleted!");
       return response.status;
