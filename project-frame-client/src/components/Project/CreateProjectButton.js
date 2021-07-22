4532fd38d13dde5bf5274d76314df3ad3fb99929
Yazan <yazana@ilsny.com>
2021-07-22 15:29:26 -0400

    moved create project button to material ui button

--- project-frame-client/src/components/Project/CreateProjectButton.js
+++ project-frame-client/src/components/Project/CreateProjectButton.js
@@ -1,11 +1,19 @@
 import React, { Fragment } from "react";
 import { Link } from "react-router-dom";
+import Button from "@material-ui/core/Button";
+import AddCircleIcon from "@material-ui/icons/AddCircle";
 
 const CreateProjectButton = () => {
   return (
     <Fragment>
-      <Link to="/addProject" className="btn btn-lg btn-dark btn-info">
-        Create a Project
+      <Link to="/addProject" style={{ textDecoration: "none" }}>
+        <Button
+          variant="contained"
+          color="primary"
+          startIcon={<AddCircleIcon />}
+        >
+          Create New Project
+        </Button>
       </Link>
     </Fragment>
   );
