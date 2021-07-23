8ee796e0cd83d73d00537622a11a740221526965
Yazan <yazana@ilsny.com>
2021-07-23 00:49:00 -0400

    Add logo and fixed error persistant issue

--- project-frame-client/src/components/UserManagement/Register.js
+++ project-frame-client/src/components/UserManagement/Register.js
@@ -1,48 +1,46 @@
-
-import React,{useState} from 'react';
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
+import React, { useState, useEffect } from "react";
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
 // import FormControlLabel from '@material-ui/core/FormControlLabel';
 // import Checkbox from '@material-ui/core/Checkbox';
-import Link from '@material-ui/core/Link';
-import Grid from '@material-ui/core/Grid';
-import Box from '@material-ui/core/Box';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
+import Link from "@material-ui/core/Link";
+import Grid from "@material-ui/core/Grid";
+import Box from "@material-ui/core/Box";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
 import { useActions } from "../../hooks/use-actions";
-import { useHistory,Link as RouterLink } from "react-router-dom";
+import { useHistory, Link as RouterLink } from "react-router-dom";
 import { useSelector } from "react-redux";
 function Copyright() {
-
   return (
     <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © '}
+      {"Copyright © "}
       <Link color="inherit" href="https://www.yazanalmatar.com/">
         YazanAlmatar.com
-      </Link>{' '}
+      </Link>{" "}
       {new Date().getFullYear()}
-      {'.'}
+      {"."}
     </Typography>
   );
 }
 
 const useStyles = makeStyles((theme) => ({
   paper: {
-    marginTop: theme.spacing(8),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
+    marginTop: theme.spacing(1),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
   },
   avatar: {
     margin: theme.spacing(1),
     backgroundColor: theme.palette.secondary.main,
   },
   form: {
-    width: '100%', // Fix IE 11 issue.
+    width: "100%", // Fix IE 11 issue.
     marginTop: theme.spacing(1),
   },
   submit: {
@@ -51,9 +49,14 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export default function SignIn() {
-    const security = useSelector((state) => state.securityState);
+  useEffect(() => {
+    return () => {
+      clearErrors();
+    };
+  }, []);
+  const security = useSelector((state) => state.securityState);
 
-  const { createNewUser } = useActions();
+  const { createNewUser, clearErrors } = useActions();
   const errors = useSelector((state) => state.errorState);
   const history = useHistory();
   const classes = useStyles();
@@ -63,7 +66,7 @@ export default function SignIn() {
     password: "",
     confirmPassword: "",
   });
-    const onChangeHandler = (e) => {
+  const onChangeHandler = (e) => {
     setUser({ ...user, [e.target.name]: e.target.value });
   };
 
@@ -74,16 +77,22 @@ export default function SignIn() {
   };
   return (
     <Container component="main" maxWidth="xs">
+      <div
+        className="logo-wrapper"
+        style={{ display: "flex", justifyContent: "center" }}
+      >
+        <img src="project-frame-logo.png" style={{ width: "140px" }} />
+      </div>
       <CssBaseline />
       <div className={classes.paper}>
-        <Avatar className={classes.avatar}>
+        {/* <Avatar className={classes.avatar}>
           <LockOutlinedIcon />
-        </Avatar>
+        </Avatar> */}
         <Typography component="h1" variant="h5">
           Sign Up
         </Typography>
-        <form className={classes.form} >
-        <TextField
+        <form className={classes.form}>
+          <TextField
             variant="outlined"
             margin="normal"
             required
@@ -92,7 +101,7 @@ export default function SignIn() {
             label="Full Name"
             name="fullName"
             autoFocus
-            error={errors.fullName !=null}
+            error={errors.fullName != null}
             helperText={errors.fullName}
             onChange={onChangeHandler}
           />
@@ -106,14 +115,14 @@ export default function SignIn() {
             name="username"
             autoComplete="email"
             autoFocus
-            error={errors.username !=null}
+            error={errors.username != null}
             helperText={errors.username}
             onChange={onChangeHandler}
           />
           <TextField
             variant="outlined"
             margin="normal"
-            error={errors.password !=null}
+            error={errors.password != null}
             helperText={errors.password}
             required
             fullWidth
@@ -127,7 +136,7 @@ export default function SignIn() {
           <TextField
             variant="outlined"
             margin="normal"
-            error={errors.password !=null}
+            error={errors.password != null}
             helperText={errors.confirmPassword}
             required
             fullWidth
@@ -149,13 +158,12 @@ export default function SignIn() {
             Sign Up
           </Button>
           <Grid container>
-            <Grid item xs>
-            </Grid>
+            <Grid item xs></Grid>
             <Grid item>
-            <RouterLink to="/login">
-              <Link  variant="body2">
-                {"You already have an account? Log In"}
-              </Link>
+              <RouterLink to="/login">
+                <Link variant="body2">
+                  {"You already have an account? Log In"}
+                </Link>
               </RouterLink>
             </Grid>
           </Grid>
