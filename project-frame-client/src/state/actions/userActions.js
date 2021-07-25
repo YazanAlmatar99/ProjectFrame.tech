ce590dab9d16ca576384d36b3e4eeb48d1b7d9b7
Yazan <yazana@ilsny.com>
2021-07-25 17:21:26 -0400

    fixed live update bug when updating user

--- project-frame-client/src/state/actions/userActions.js
+++ project-frame-client/src/state/actions/userActions.js
@@ -1,10 +1,11 @@
 import axios from "axios";
 import { toast } from "react-toastify";
-import { UPDATE_USER_INFO } from "../action-types";
+import { SET_CURRENT_USER } from "../action-types";
 
 export const updateUserInfo = (updatedUser) => async (dispatch) => {
   try {
     const response = await axios.patch("/api/users", updatedUser);
+    dispatch({ type: SET_CURRENT_USER, payload: response.data });
     toast.success("You successfully updated your profile!");
   } catch (error) {
     toast.error("Failed updating user details!");
